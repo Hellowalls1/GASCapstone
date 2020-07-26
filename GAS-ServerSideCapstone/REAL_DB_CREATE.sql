37d20fc29fdb7a78823f21b45f85402f8257f372
Hellowalls1 <thesingingbutcher@gmail.com>
2020-07-25 21:56:53 -0500

    seed data

---
+++ GAS-ServerSideCapstone/REAL_DB_CREATE.sql
@@ -0,0 +1,63 @@
+﻿USE [master]
+
+IF db_id('Gas') IS NULl
+  CREATE DATABASE [Gas]
+GO
+
+USE [Gas]
+GO
+
+
+DROP TABLE IF EXISTS [Comment];
+DROP TABLE IF EXISTS [Item];
+DROP TABLE IF EXISTS [Category];
+DROP TABLE IF EXISTS [User];
+
+
+GO
+
+
+
+CREATE TABLE [User] (
+  [Id] integer PRIMARY KEY IDENTITY NOT NULL,
+  [FirebaseUserId] nvarchar(50) NOT NULL,
+  [FirstName] nvarchar(50) NOT NULL,
+  [LastName] nvarchar(50) NOT NULL,
+  [Email] nvarchar(555) NOT NULL
+
+   CONSTRAINT UQ_FirebaseUserId UNIQUE(FirebaseUserId)
+)
+GO
+
+CREATE TABLE [Category] (
+  [Id] int PRIMARY KEY IDENTITY NOT NULL,
+  [Title] nvarchar(50) NOT NULL
+)
+ GO
+
+CREATE TABLE [Item] (
+  [Id] integer PRIMARY KEY IDENTITY NOT NULL,
+  [UserId] integer NOT NULL,
+  [CategoryId] integer NOT NULL,
+  [Title] nvarchar(50) NOT NULL,
+  [Description] nvarchar(max) NOT NULL,
+  [IsForSale] bit NOT NULL,
+  [SalePrice] nvarchar(50),
+  [ImageUrl] nvarchar(max) NOT NULL
+
+  CONSTRAINT [FK_Item_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]),
+  CONSTRAINT [FK_Item_Category] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id])
+)
+GO
+
+CREATE TABLE [Comment] (
+  [Id] integer PRIMARY KEY IDENTITY NOT NULL,
+  [ItemId] integer NOT NULL,
+  [UserId] integer NOT NULL,
+  [Title] nvarchar(50) NOT NULL,
+  [Description] nvarchar(max) NOT NULL
+
+  CONSTRAINT [FK_Comment_Item] FOREIGN KEY ([ItemId]) REFERENCES [Item] ([Id]),
+  CONSTRAINT [FK_Comment_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
+)
+GO
---
+++ GAS-ServerSideCapstone/REAL_DB_SEED.sql
@@ -0,0 +1,36 @@
+﻿
+set identity_insert [User] on
+insert into [User] (Id, FirebaseUserId, FirstName, LastName, Email) values (1, '00sUFXwjjFf2WL5KZozXXXWcnBj1', 'Erykah', 'Sanford', 'erykah@erykah.com');
+insert into [User] (Id, FirebaseUserId, FirstName, LastName, Email) values (2, 'Do0QqiO9ImY844uKcThTuv5bzpD2', 'Tim', 'Jones', 'tim@tim.com');
+insert into [User] (Id, FirebaseUserId, FirstName, LastName, Email) values (3, 'VDvuX0VqnOfzUawFVnTW7wfJYi33', 'John', 'Maplethorpe', 'john@john.com');
+insert into [User] (Id, FirebaseUserId, FirstName, LastName, Email) values (4, 'ubiEyDxUJuPDpc4YBu9qbbLShAX2', 'Eva', 'Mendez', 'eva@eva.com');
+
+set identity_insert [User] off
+
+set identity_insert [Category] on
+insert into [Category] ([Id], [Title]) 
+VALUES (1, 'Guitar'), (2, 'Bass'), (3, 'Drums'), (4, 'Recording'), (5, 'Keys/Synth'), (6, 'Live Sound'), (7, 'Amp'), (8, 'Pedal'), (9, 'Acoustic Instrument');
+set identity_insert [Category] off
+
+set identity_insert [Item] on
+
+insert into [Item] ([Id], [UserId], [CategoryId], [Title], [Description], [IsForSale], [SalePrice], [ImageUrl]) values (1, 2, 1, '1989 Fender Stratocaster', 'Guitar is Candy Apple Red. It has upgraded Dimarzio pickups and a completely overhauled wiring harness.', 1, '1500', 'https://2.bp.blogspot.com/-zLOSPWQ7dPc/UX3WY0JK_wI/AAAAAAAAJuk/I4rX1kxRJk0/s400/63+candy+apple+strat.JPG');
+insert into [Item] ([Id], [UserId], [CategoryId], [Title], [Description], [IsForSale], [SalePrice], [ImageUrl]) values (2, 3, 7, 'Crate GT212', '120 Watt Combo Amp. Do not H8 the Cr8', 1, '2000', 'https://images.reverb.com/image/upload/s--RclooOh4--/t_card-square/v1568637603/mz9lk39capetodj4idn1.jpg');
+insert into [Item] ([Id], [UserId], [CategoryId], [Title], [Description], [IsForSale], [SalePrice], [ImageUrl]) values (3, 2, 2, 'MIM FENDER P BASS', 'Navy Blue. Bought this on my 18th Birthday. Upgraded the Pickups to Lollars. Built in 2006-2007', 0, NULL, 'https://3.bp.blogspot.com/_ETgjuuN-R-I/S1cu8JYFZjI/AAAAAAAAAZA/VGTs71yCYF0/s400/fender+bass+guitar.jpg'); 
+insert into [Item] ([Id], [UserId], [CategoryId], [Title], [Description], [IsForSale], [SalePrice], [ImageUrl]) values (4, 3, 3, 'Slingerland Drum Kit', '1968 Brown Vintage Kit. Normal wear and tear on an older kit', 0, NULL, 'https://www.vintagedrum.com/wp-content/uploads/2016/04/VI-2936-Sling-4pc-mahog.jpg');
+insert into [Item] ([Id], [UserId], [CategoryId], [Title], [Description], [IsForSale], [SalePrice], [ImageUrl]) values (5, 1, 1, 'Gibson SG', '1969 Vintage SG. Normal wear and tear on an older guitar', 0, NULL, 'https://i.ebayimg.com/images/g/9XYAAOSwe8ldEla9/s-l300.jpg');
+insert into [Item] ([Id], [UserId], [CategoryId], [Title], [Description], [IsForSale], [SalePrice], [ImageUrl]) values (6, 4, 1, 'Gibson Firebird', '1965 Vintage Firebird. Normal wear and tear on an older guitar', 0, NULL, 'https://www.vintageandrare.com/uploads/products/43180/1650512/big.jpg');
+set identity_insert [Item] off
+
+set identity_insert [Comment] on
+insert into [Comment] ([Id], [ItemId], [UserId], [Title], [Description]) values (1, 1, 1, 'Killer Guitar', 'What is your asking Price?');
+insert into [Comment] ([Id], [ItemId], [UserId], [Title], [Description]) values (2, 1, 2, 'Killer Guitar', 'My asking price is $1500.');
+insert into [Comment] ([Id], [ItemId], [UserId], [Title], [Description]) values (3, 1, 1, 'Killer Guitar', 'Yea! My number is 666-666-6666. Shoot me a text and we can work it out!'); 
+insert into [Comment] ([Id], [ItemId], [UserId], [Title], [Description]) values (5, 2, 2, 'Trades for Amp?', 'Are you open to any trades?');
+insert into [Comment] ([Id], [ItemId], [UserId], [Title], [Description]) values (6, 2, 3, 'Trades for Amp?', 'Only if you have a Princeton Reverb.');
+insert into [Comment] ([Id], [ItemId], [UserId], [Title], [Description]) values (7, 2, 2, 'Trades for Amp?', 'Guess it was not meant to be. Good luck!');
+insert into [Comment] ([Id], [ItemId], [UserId], [Title], [Description]) values (8, 2, 1, 'Trades for Amp?', 'I have a 1968 Princeton Reverb Reissue.'); 
+insert into [Comment] ([Id], [ItemId], [UserId], [Title], [Description]) values (9, 2, 3, 'Trades for Amp?', 'Definately interested. Call me. 555-555-5555.');
+set identity_insert [Comment] off
+
+

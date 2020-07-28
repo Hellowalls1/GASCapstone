111c30aec878e0714eddd92736bc55b653f2869f
Hellowalls1 <thesingingbutcher@gmail.com>
2020-07-28 11:36:49 -0500

    modal form opening, closing, updating, and posting to the datebase. Issue with bit

--- GAS-ServerSideCapstone/client/gas-serversidecapstone/src/components/Item.js
+++ GAS-ServerSideCapstone/client/gas-serversidecapstone/src/components/Item.js
@@ -18,10 +18,40 @@ import { useHistory } from "react-router-dom";
 //using the Card component that comes with reactstrap to organize some of the post details
 const Item = ({ item }) => {
   const { deleteItem, updateItem } = useContext(ItemContext);
+  const { categories } = useContext(CategoryContext);
+
   const [deleteModal, setDeleteModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
+
+  const [userId, setUserId] = useState(item.user.id);
+  const [categoryId, setCategoryId] = useState(item.category.id);
+  const [title, setTitle] = useState(item.title);
+  const [description, setDescription] = useState(item.description);
+  const [isForSale, setIsForSale] = useState(item.isForSale);
+  const [salePrice, setSalePrice] = useState(item.salePrice);
+  const [imageUrl, setImageUrl] = useState(item.imageUrl);
+
   const toggleDelete = () => {
     setDeleteModal(!deleteModal);
   };
+
+  const toggleEdit = () => {
+    setEditModal(!editModal);
+  };
+
+  const submitForm = () => {
+    updateItem({
+      id: item.id,
+      // userId: user.id,
+      categoryId: parseInt(categoryId),
+      title: title,
+      description: description,
+      imageUrl: imageUrl,
+      isForSale: isForSale,
+      salePrice: salePrice,
+    });
+  };
+
   return (
     <>
       <Card className="m-4">
@@ -37,20 +67,10 @@ const Item = ({ item }) => {
             {item.isForSale === true
               ? `Yes. It costs $${item.salePrice}`
               : "Item is not for sale"}
-            {/* 
-            {(() => {
-              switch (item.isForSale) {
-                case "false":
-                  return "Item is not for sale";
-                case "true":
-                  return "Item is  for sale";
-                default:
-                  return "Item is not for sale";
-              }
-            })()} */}
           </p>
         </CardBody>
         <Button onClick={toggleDelete}>Delete</Button>
+        <Button onClick={toggleEdit}>Edit</Button>
       </Card>
 
       <Modal isOpen={deleteModal} toggle={toggleDelete}>
@@ -80,6 +100,121 @@ const Item = ({ item }) => {
                 onClick={toggleDelete}
               >
                 No
+              </Button>
+            </div>
+          </div>
+        </ModalBody>
+      </Modal>
+
+      <Modal isOpen={editModal} toggle={toggleEdit}>
+        <ModalBody>
+          <div className="form-group">
+            <label htmlFor="category">Category: </label>
+            <select
+              id="category"
+              onChange={(e) => setCategoryId(e.target.value)}
+              required
+              autoFocus
+              className="form-control mt-4"
+              defaultValue={item.categoryId}
+            >
+              <option key="0" value="0">
+                Select A Category
+              </option>
+              {categories.map((c) => (
+                <option value={c.id} key={c.id}>
+                  {c.title}
+                </option>
+              ))}
+            </select>
+
+            <label htmlFor="title">Title: </label>
+            <input
+              type="text"
+              id="title"
+              onChange={(e) => setTitle(e.target.value)}
+              required
+              autoFocus
+              className="form-control mt-4"
+              defaultValue={item.title}
+            />
+
+            <label htmlFor="description">Description: </label>
+            <input
+              type="text-area"
+              id="content"
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              autoFocus
+              className="form-control mt-4"
+              defaultValue={item.description}
+            />
+
+            <label htmlFor="isForSale">Is Item For Sale? </label>
+            <select
+              id="isForSale"
+              onChange={(e) => setIsForSale(e.target.value)}
+              required
+              autoFocus
+              className="form-control mt-4"
+              // defaultValue={item.isForSale}
+            >
+              <option key="0" value="0">
+                No
+              </option>
+              <option key="1" value="1">
+                Yes
+              </option>
+              )
+            </select>
+
+            <label htmlFor="salePrice">Sale Price: </label>
+            <input
+              type="text-area"
+              id="salePrice"
+              onChange={(e) => setSalePrice(e.target.value)}
+              required
+              autoFocus
+              className="form-control mt-4"
+              defaultValue={item.salePrice}
+            />
+
+            <label htmlFor="imageUrl">Image URL: </label>
+            <input
+              type="text"
+              id="imageUrl"
+              onChange={(e) => setImageUrl(e.target.value)}
+              autoFocus
+              className="form-control mt-4"
+              defaultValue={item.imageUrl}
+            />
+
+            <div className="">
+              <Button
+                type="submit"
+                size="sm"
+                color="info"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  if (categoryId === "0") {
+                    window.alert("You forgot a category!");
+                  } else if (!title) {
+                    window.alert("You forgot a title!");
+                  } else if (!description) {
+                    window.alert("You forgot a description!");
+                  } else if (!salePrice) {
+                    window.alert("You forgot a sale price!");
+                  } else if (!imageUrl) {
+                    window.alert("You forgot a image url!");
+                  } else {
+                    submitForm(item);
+
+                    toggleEdit();
+                  }
+                }}
+                className="btn mt-4"
+              >
+                Save
               </Button>
             </div>
           </div>
